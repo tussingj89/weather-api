9fce4a8abe09f5fcd5ecb2363223c19abc039e23
tussingj89 <58670450+tussingj89@users.noreply.github.com>
2020-12-13 11:58:19 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,64 @@
+// variables and url calls 
+var cityInput = $("#city-input");
+var search = $("#go");
+var history = $("#history")
+var clearHistory = ("#clear-Button")
+var cityName = $("#city-name");
+var currentImg = $("#currentImg");
+var temp = $("#temperature");
+var humidity = ("#humidity");
+var windSpeed = $("#windSpeed");
+var UVIndex = $("#UV-index");
+// var currentDate = 
+
+
+
+var status = "Fahrenheit";
+var historystorage = [];
+
+search.on('click', function(event) {
+    event.preventDefault();
+        citySearch($(this).text());
+});
+
+function citySearch(cityInput) {
+    
+var queryURL= "api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=fba36da2ac563b2d05033368e455a294";
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+}).then(function (response) {
+    console.log(response);
+    console.log(queryURL);
+
+    cityName = $("<h2>").text(response.name);
+    //  = cityInput.append(" " + mainDate);
+    temp= $("<p>").text("Tempraturer: " + response.main.temp);
+    humidity = $("<p>").text("Humidity: " + response.main.humidity);
+    windSpeed = $("<p>").text("Wind Speed: " + response.wind.speed);
+    // UVIndex = $("<p>").text("UV Index: " + response.)
+    var currentweather = response.weather[0].main;
+
+    if (currentweather === "Rain") {
+        var currentImg = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+        currentImg.attr("style", "height: 60px; width: 60px");
+    }
+    else if (currentweather === "Clouds") {
+        var currentImg = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+        currentImg.attr("style", "height: 60px; width: 60px");
+    } 
+    else if (currentweather === "Clear") {
+        var currentImg = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+        currentImg.attr("style", "height: 60px; width: 60px");
+    }
+    else if (currentweather === "Drizzle") {
+        var currentImg = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+        currentImg.attr("style", "height: 60px; width: 60px");
+    }
+    else if (currentweather === "Snow") {
+        var currentImg = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+        currentImg.attr("style", "height: 60px; width: 60px");
+     }
+});
+}
